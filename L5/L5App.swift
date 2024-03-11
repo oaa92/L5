123bffd9bef51ff8b753705bd9f4be2db2925eb9
Anatoliy Odinetskiy <aodinetsky@yandex-team.ru>
2024-03-11 22:18:04 +0700

    Initial Commit

---
+++ L5/L5App.swift
@@ -0,0 +1,17 @@
+//
+//  L5App.swift
+//  L5
+//
+//  Created by Anatoliy Odinetskiy on 11.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct L5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
